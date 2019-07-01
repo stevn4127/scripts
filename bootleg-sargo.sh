3c62d97248ba1c8314a4b34583135d7a204ce02a
stebomurkn420 <stevn4127@gmail.com>
2019-07-01 08:42:45 -0400

    rename and clean up

--- bootleg-sargo.sh
+++ bootleg-sargo.sh
@@ -35,7 +35,7 @@ git clone https://github.com/stebomurkn420/device_google_sargo device/google/sar
 git clone https://github.com/stebomurkn420/device_google_bonito device/google/bonito
 git clone https://github.com/stebomurkn420/device_google_bonito-sepolicy device/google/bonito-sepolicy
 git clone https://github.com/ZVNexus/android_vendor_google vendor/google
-git clone https://github.com/DirtyUnicorns/android_kernel_google_bonito kernel/google/bonito
+git clone https://github.com/stebomurkn420/Dank_kernel_google_bonito kernel/google/bonito
 git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
 git clone https://github.com/DirtyUnicorns/android_hardware_google_pixel hardware/google/pixel
 git clone https://gitlab.com/stebomurkn420/vendor_gapps.git vendor/gapps
