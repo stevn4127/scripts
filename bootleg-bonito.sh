c2d5478c332d91f30b6c29927b803fca9c15de78
stebomurkn420 <stevn4127@gmail.com>
2019-07-04 03:04:40 -0400

    Update bootleg-bonito.sh

--- bootleg-bonito.sh
+++ bootleg-bonito.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 
+sudo apt install repo
+
+sudo apt install libncurses5
+
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
+sudo apt install openjdk-8-jdk
+
+cd
+
+mkdir bootleg
+
+mkdir -p ~/bin
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+chmod a+x ~/bin/repo
+
 cd $HOME/bootleg
+
+repo init -u https://github.com/BootleggersROM/manifest.git -b pasta
+
+repo sync --force-sync -j32
+
+rm -rf device/google/bonito
 rm -rf hardware/qcom/audio
+rm -rf device/google/sargo
+rm -rf device/google/bonito-sepolicy
+rm -rf vendor/google
+rm -rf kernel/google/bonito
+rm -rf hardware/google/pixel
+rm -rf vendor/gapps
+rm -rf hardware/qcom/sdm710
 
 git clone https://github.com/stebomurkn420/device_google_bonito device/google/bonito
 git clone https://github.com/stebomurkn420/device_google_bonito-sepolicy device/google/bonito-sepolicy
