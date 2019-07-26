00eb14cc5748f5c7b1b1bfe71265eded27691e59
stebomurkn420 <stevn4127@gmail.com>
2019-07-26 02:19:23 -0400

    Update blueline script

--- blueline.sh
+++ blueline.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
+cd
+
+sudo apt install repo
+
+sudo apt install libncurses5
+
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
+sudo apt install openjdk-8-jdk
+
+rm -rf bootleg
+
+cd
+
+mkdir bootleg
+
+mkdir -p ~/bin
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+chmod a+x ~/bin/repo
+
 cd $HOME/bootleg
+
+repo init -u https://github.com/BootleggersROM/manifest.git -b pasta
+
+repo sync --force-sync -j32
 
 rm -rf hardware/qcom/audio
 rm -rf hardware/qcom/sdm845/data
