53dffe0fabae783cb9ae6024788ae31d08147fc0
stebomurkn420 <stevn4127@gmail.com>
2019-08-13 21:44:37 -0400

    Bonito: update liquid build script

--- liquid-bonito.sh
+++ liquid-bonito.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt install repo
+cd
 
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-
-sudo apt install openjdk-8-jdk
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
 
 cd
 
@@ -27,16 +25,14 @@ rm -rf device/google/bonito
 rm -rf kernel/google/bonito
 rm -rf vendor/google
 rm -rf kernel/google/bonito
-rm -rf hardware/google/pixel
 rm -rf vendor/gapps
 rm -rf hardware/qcom/sdm710
 
 git clone https://github.com/stebomurkn420/device_google_bonito device/google/bonito -b liquid
 git clone https://github.com/stebomurkn420/device_google_bonito-sepolicy device/google/bonito-sepolicy
 git clone https://github.com/stebomurkn420/vendor_google -b liquid vendor/google
-git clone --recurse-submodules https://github.com/stebomurkn420/Dank_kernel_google_bonito kernel/google/bonito
+git clone --recurse-submodules https://github.com/stebomurkn420/Dank_sarnito -b inline kernel/google/bonito
 git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
-git clone https://github.com/DirtyUnicorns/android_hardware_google_pixel hardware/google/pixel
 git clone https://gitlab.com/stebomurkn420/vendor_gapps vendor/gapps
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r45 hardware/qcom/sdm710/display
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/gps -b android-9.0.0_r45 hardware/qcom/sdm710/gps
