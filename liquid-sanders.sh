945550f181eb44524b79354f0a3a108be1bb2ad6
stebomurkn420 <stevn4127@gmail.com>
2019-08-17 17:25:37 -0400

    Sanders: update liquid build script

--- liquid-sanders.sh
+++ liquid-sanders.sh
@@ -16,6 +16,14 @@ chmod a+x ~/bin/repo
 
 cd $HOME/liquid
 
+rm -rf hardware/qcom/audio
+rm -rf device/google/bonito
+rm -rf kernel/google/bonito
+rm -rf vendor/google
+rm -rf kernel/google/bonito
+rm -rf vendor/gapps
+rm -rf hardware/qcom/sdm710
+
 git clone https://github.com/LiquidRemixSanders/device_motorola_sanders.git -b 9x device/motorola/sanders
 git clone https://github.com/LiquidRemixSanders/vendor_motorola_sanders.git -b 9.0 vendor/motorola/sanders
 git clone https://github.com/LiquidRemixSanders/android_kernel_motorola_msm8953.git -b 9.0-eas-caf kernel/motorola/msm8953
