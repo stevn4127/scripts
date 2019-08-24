b4e6c23510f50bd154f7ce33cd31942112c67a35
stebomurkn420 <stevn4127@gmail.com>
2019-08-23 23:27:05 -0400

    Bluecross: Update Bootleggers ROM build script

--- blueline.sh
+++ blueline.sh
@@ -2,19 +2,7 @@
 
 cd
 
-sudo apt install repo
-
-sudo apt install libncurses5
-
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-
-sudo apt install openjdk-8-jdk
-
-rm -rf bootleg
-
-cd
-
-mkdir bootleg
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
 
 mkdir -p ~/bin
 
@@ -22,11 +10,13 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 
 chmod a+x ~/bin/repo
 
+mkdir bootleg
+
 cd $HOME/bootleg
 
 repo init -u https://github.com/BootleggersROM/manifest.git -b pasta
 
-repo sync --force-sync -j32
+repo sync -f --force-sync --no-clone-bundle -j32
 
 rm -rf hardware/qcom/audio
 rm -rf hardware/qcom/sdm845/data
@@ -36,14 +26,19 @@ rm -rf kernel/google/bluecross
 rm -rf device/google/crosshatch-sepolicy
 rm -rf vendor/google
 
-git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
-git clone https://gitlab.com/stebomurkn420/vendor_gapps vendor/gapps
-git clone https://github.com/stebomurkn420/device_google_crosshatch device/google/crosshatch
 git clone https://github.com/stebomurkn420/device_google_blueline device/google/blueline
-git clone https://github.com/stebomurkn420/kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/stebomurkn420/android_device_google_crosshatch -b pasta device/google/crosshatch
 git clone https://github.com/stebomurkn420/device_google_crosshatch-sepolicy device/google/crosshatch-sepolicy
-git clone https://github.com/choose-a/android_hardware_qcom_sdm845_data_ipacfg-mgr hardware/qcom/sdm845/data/ipacfg-mgr
 git clone https://github.com/DirtyUnicorns/android_vendor_google vendor/google
+git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
+git clone https://gitlab.com/stebomurkn420/vendor_gapps.git vendor/pixelgapps
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/display -b android-9.0.0_r46 hardware/qcom/sdm845/display
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/gps -b android-9.0.0_r46 hardware/qcom/sdm845/gps
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/media -b android-9.0.0_r46 hardware/qcom/sdm845/media
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/thermal -b android-9.0.0_r46 hardware/qcom/sdm845/thermal
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/vr -b android-9.0.0_r46 hardware/qcom/sdm845/vr
+git clone https://github.com/stebomurkn420/android_hardware_qcom_sdm845_data_ipacfg-mgr hardware/qcom/sdm845/data/ipacfg-mgr
 
 . build/envsetup.sh
 lunch bootleg_blueline-userdebug
--- bootleg-crosshatch.sh
+++ bootleg-crosshatch.sh
@@ -4,6 +4,12 @@ cd
 
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
 
+mkdir -p ~/bin
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+chmod a+x ~/bin/repo
+
 mkdir bootleg
 
 cd $HOME/bootleg
@@ -26,11 +32,11 @@ git clone https://github.com/DirtyUnicorns/android_vendor_google vendor/google
 git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
 git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
 git clone https://gitlab.com/stebomurkn420/vendor_gapps.git vendor/pixelgapps
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r45 hardware/qcom/sdm845/display
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/gps -b android-9.0.0_r45 hardware/qcom/sdm845/gps
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/media -b android-9.0.0_r45 hardware/qcom/sdm845/media
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r45 hardware/qcom/sdm845/thermal
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r45 hardware/qcom/sdm845/vr
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/display -b android-9.0.0_r45 hardware/qcom/sdm845/display
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/gps -b android-9.0.0_r45 hardware/qcom/sdm845/gps
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/media -b android-9.0.0_r45 hardware/qcom/sdm845/media
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/thermal -b android-9.0.0_r45 hardware/qcom/sdm845/thermal
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm845/vr -b android-9.0.0_r45 hardware/qcom/sdm845/vr
 git clone https://github.com/stebomurkn420/android_hardware_qcom_sdm845_data_ipacfg-mgr hardware/qcom/sdm845/data/ipacfg-mgr
 
 . build/envsetup.sh
