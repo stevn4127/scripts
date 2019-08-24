b9b9d6ed54bde711d7faf1a1e34cd4feb9811c56
stebomurkn420 <stevn4127@gmail.com>
2019-08-24 04:29:39 -0400

    Update hardware/qcom/sdm710 to r47

--- dank.sh
+++ dank.sh
@@ -10,11 +10,11 @@ mkdir sarnito-zips
 
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r44 aarch64-linux-android-4.9
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r44 arm-linux-androideabi-4.9
-git clone https://github.com/AOSiP/platform_prebuilts_clang_host_linux-x86 clang
+git clone https://github.com/Haseo97/Clang-10.0.0 clang
 git clone --recurse-submodules https://github.com/stebomurkn420/Dank_sarnito -b anykernel sarnito
 git clone https://github.com/stebomurkn420/AnyKernel3 AnyKernel3
 
 cd sarnito
 
-bash dank-kernel.sh
+bash build.sh
  
--- liquid-bonito.sh
+++ liquid-bonito.sh
@@ -34,11 +34,11 @@ git clone https://github.com/stebomurkn420/vendor_google -b liquid vendor/google
 git clone --recurse-submodules https://github.com/stebomurkn420/Dank_sarnito -b inline kernel/google/bonito
 git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
 git clone https://gitlab.com/stebomurkn420/vendor_gapps vendor/gapps
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r45 hardware/qcom/sdm710/display
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/gps -b android-9.0.0_r45 hardware/qcom/sdm710/gps
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/media -b android-9.0.0_r45 hardware/qcom/sdm710/media
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r45 hardware/qcom/sdm710/thermal
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r45 hardware/qcom/sdm710/vr
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r47 hardware/qcom/sdm710/display
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/gps -b android-9.0.0_r47 hardware/qcom/sdm710/gps
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/media -b android-9.0.0_r47 hardware/qcom/sdm710/media
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r47 hardware/qcom/sdm710/thermal
+git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r47 hardware/qcom/sdm710/vr
 git clone https://github.com/stebomurkn420/hardware_qcom_sdm710_data_ipacfg-mgr hardware/qcom/sdm710/data/ipacfg-mgr
 
 . build/envsetup.sh
