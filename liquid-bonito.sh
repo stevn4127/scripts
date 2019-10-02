37fc83251208edfd5a5c1248bccdab4b61eb0021
stebomurkn420 <stevn4127@gmail.com>
2019-10-02 03:03:24 -0400

    Update liquid-bonito.sh

--- liquid-bonito.sh
+++ liquid-bonito.sh
@@ -2,11 +2,6 @@
 
 cd
 
-# Functions
-function clean_all {
-		make clean
-}
-
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
 
 cd
@@ -26,19 +21,21 @@ repo init -u git://github.com/LiquidRemix/android_manifest.git -b ten
 repo sync --force-sync -j32
 
 rm -rf device/google/bonito
+rm -rf device/google/sargo
+rm -rf device/google/bonito-sepolicy
 rm -rf device/qcom
-rm -rf kernel/google/bonito
-rm -rf vendor/google
-rm -rf kernel/google/bonito
-rm -rf vendor/gapps
 rm -rf hardware/qcom/sdm710
 rm -rf hardware/qcom/sdm845
+rm -rf kernel/google/bonito
+rm -rf vendor/google
+rm -rf vendor/images
+rm -rf vendor/gapps
 
 git clone https://github.com/stebomurkn420/Bonito -b ten device/google/bonito
 git clone https://github.com/stebomurkn420/Sargo -b ten device/google/sargo
 git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r2 device/google/bonito-sepolicy
 git clone https://github.com/UnicornioSucio/android_vendor_google vendor/google
-git clone https://github.com/UnicornioSucio/android_kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
 git clone https://gitlab.com/stebomurkn420/vendor_images.git -b ten vendor/images
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r47 hardware/qcom/sdm710/display
@@ -50,28 +47,6 @@ git clone https://github.com/stebomurkn420/hardware_qcom_sdm710_data_ipacfg-mgr 
 
 . build/envsetup.sh
 
-echo
-
-while read -p "Do you want to make clean (y/n)? " cchoice
-do
-case "$cchoice" in
-	y|Y )
-		clean_all
-		echo
-		echo "All Cleaned now."
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
-
 lunch liquid_bonito-userdebug
 
-mka bacon -j32 |& tee /tmp/build.log
+make liquid -j32 |& tee /tmp/build.log
