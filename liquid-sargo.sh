ba40b29c530827942bbe83a99e31d8f6abf8d749
stebomurkn420 <stevn4127@gmail.com>
2019-10-02 03:19:38 -0400

    Update liquid-sargo.sh

--- liquid-sargo.sh
+++ liquid-sargo.sh
@@ -26,19 +26,21 @@ repo init -u git://github.com/LiquidRemix/android_manifest.git -b ten
 repo sync --force-sync -j32
 
 rm -rf device/google/bonito
+rm -rf device/google/sargo
+rm -rf device/google/bonito-sepolicy
 rm -rf device/qcom
-rm -rf kernel/google/bonito
-rm -rf vendor/google
-rm -rf kernel/google/bonito
-rm -rf vendor/gapps
 rm -rf hardware/qcom/sdm710
 rm -rf hardware/qcom/sdm845
+rm -rf kernel/google/bonito
+rm -rf vendor/google
+rm -rf vendor/images
+rm -rf vendor/gapps
 
 git clone https://github.com/stebomurkn420/Bonito -b ten device/google/bonito
 git clone https://github.com/stebomurkn420/Sargo -b ten device/google/sargo
 git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r2 device/google/bonito-sepolicy
 git clone https://github.com/UnicornioSucio/android_vendor_google vendor/google
-git clone https://github.com/UnicornioSucio/android_kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
 git clone https://gitlab.com/stebomurkn420/vendor_images.git -b ten vendor/images
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r47 hardware/qcom/sdm710/display
