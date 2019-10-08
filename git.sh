82ee560bc3210e8fe3944c4b302bf1a7411939fb
stebomurkn420 <stevn4127@gmail.com>
2019-10-08 01:42:23 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,23 +2,11 @@
 
 echo "This script should be ran from the root directory of your ROM's source. This script will clone the necessary repositories for building a ROM for the Pixel 3a (Sargo) and Pixel 3a XL (Bonito). The only repo that should need modified is the device tree which needs edited for your ROM's source. Hope this helps!"
 
-
-rm -rf device/google/bonito
-rm -rf device/google/sargo
-rm -rf device/google/bonito-sepolicy
-rm -rf device/qcom
-rm -rf hardware/qcom/sdm710
-rm -rf hardware/qcom/sdm845
-rm -rf kernel/google/bonito
-rm -rf vendor/google
-rm -rf vendor/images
-rm -rf vendor/gapps
-
 git clone https://github.com/stebomurkn420/Bonito -b ten device/google/bonito
 git clone https://github.com/stebomurkn420/Sargo -b ten device/google/sargo
 git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r2 device/google/bonito-sepolicy
 git clone https://github.com/DirtyUnicorns/android_vendor_google vendor/google
-git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/stebomurkn420/kernel_google_bluecross kernel/google/bluecross
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
 git clone https://gitlab.com/stebomurkn420/vendor_images.git -b ten vendor/images
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r47 hardware/qcom/sdm710/display
--- liquid-bonito.sh
+++ liquid-bonito.sh
@@ -34,8 +34,8 @@ rm -rf vendor/gapps
 git clone https://github.com/stebomurkn420/Bonito -b ten device/google/bonito
 git clone https://github.com/stebomurkn420/Sargo -b ten device/google/sargo
 git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r2 device/google/bonito-sepolicy
-git clone https://github.com/UnicornioSucio/android_vendor_google vendor/google
-git clone https://github.com/DirtyUnicorns/android_kernel_google_bluecross kernel/google/bluecross
+git clone https://github.com/DirtyUnicorns/android_vendor_google vendor/google
+git clone https://github.com/stebomurkn420/kernel_google_bluecross kernel/google/bluecross
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
 git clone https://gitlab.com/stebomurkn420/vendor_images.git -b ten vendor/images
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/display -b android-9.0.0_r47 hardware/qcom/sdm710/display
