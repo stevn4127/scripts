5263590a65b81f1ba866a0185cd3d81fe5d4623e
stebomurkn420 <stevn4127@gmail.com>
2019-11-13 19:14:15 -0500

    B4S4: Update Havoc OS build script for Android 10

--- havoc-b4s4.sh
+++ havoc-b4s4.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-echo "This script should be ran from the root directory of your ROM's source. This script will clone the necessary repositories for building a ROM for the Pixel 3a (Sargo) and Pixel 3a XL (Bonito). The only repo that should need modified is the device tree which needs edited for your ROM's source. Hope this helps!"
+cd
+
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
+
+cd
+
+mkdir havoc
+
+mkdir -p ~/bin
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+chmod a+x ~/bin/repo
+
+cd $HOME/havoc
+
+repo init -u https://github.com/Havoc-OS/android_manifest.git -b ten
+
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 rm -rf device/google/bonito
 rm -rf device/google/bonito-kernel
@@ -18,10 +36,17 @@ git clone https://android.googlesource.com/device/google/bonito-sepolicy -b andr
 git clone https://github.com/TheMuppets/proprietary_vendor_google vendor/google
 git clone https://github.com/LineageOS/android_kernel_google_msm-4.9 kernel/google/msm-4.9
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
-#git clone https://gitlab.com/DirtyUnicorns/android_vendor_images.git vendor/images
 git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_display hardware/qcom/sdm710/display
 git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_gps hardware/qcom/sdm710/gps
 git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_media hardware/qcom/sdm710/media
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r47 hardware/qcom/sdm710/thermal
 git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r47 hardware/qcom/sdm710/vr
-git clone https://github.com/stebomurkn420/hardware_qcom_sdm710_data_ipacfg-mgr hardware/qcom/sdm710/data/ipacfg-mgr
+git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_data_ipacfg-mgr -b bliss hardware/qcom/sdm710/data/ipacfg-mgr
+
+. build/envsetup.sh
+
+export USE_CCACHE=1
+export CCACHE_COMPRESS=1
+export CCACHE_EXEC=/usr/bin/ccache
+
+brunch bonito |& tee /tmp/build.log
