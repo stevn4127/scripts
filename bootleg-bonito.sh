ae1ce92bde670e284a8e7a1419d79d1f20be3057
stebomurkn420 <stevn4127@gmail.com>
2019-11-25 23:34:46 -0500

    Dank kernel

--- bootleg-bonito.sh
+++ bootleg-bonito.sh
@@ -34,7 +34,7 @@ rm -rf vendor/gapps
 git clone https://github.com/stebomurkn420/Bonito -b bootleg device/google/bonito
 git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r12 device/google/bonito-sepolicy
 git clone https://github.com/TheMuppets/proprietary_vendor_google vendor/google
-git clone https://github.com/LineageOS/android_kernel_google_msm-4.9 kernel/google/msm-4.9
+git clone https://github.com/stebomurkn420/kernel_google_b4s4 kernel/google/b4s4
 git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
 git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_display hardware/qcom/sdm710/display
 git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_gps hardware/qcom/sdm710/gps
