200cdf4b379ae03c3104838a653b4f5b2b992d59
stebomurkn420 <stevn4127@gmail.com>
2019-12-01 07:43:08 -0500

    Update Havoc script

--- havoc-b4s4.sh
+++ havoc-b4s4.sh
@@ -4,6 +4,9 @@ cd
 
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk adb fastboot
 
+#git config --global user.email
+#git config --global user.name
+
 cd
 
 mkdir havoc
--- simple.sh
+++ simple.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd
+
 sudo apt install bc bison build-essential repo git ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
 
 cd
