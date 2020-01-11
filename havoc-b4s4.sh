73133984548d5acc1e41a16c500e3bd36d09484c
stebomurkn420 <stevn4127@gmail.com>
2020-01-10 21:31:10 -0500

    Havoc OS: Update build script (Should build clean and boot now for B4S4)

--- havoc-b4s4.sh
+++ havoc-b4s4.sh
@@ -28,7 +28,7 @@ rm -rf device/google/bonito-kernel
 rm -rf device/google/sargo
 rm -rf device/google/bonito-sepolicy
 rm -rf hardware/qcom/sdm710
-rm -rf hardware/qcom/sdm845
+#rm -rf hardware/qcom/sdm845 (Use LOS sdm845 repos for havoc builds)
 rm -rf kernel/google/bonito
 rm -rf vendor/google
 rm -rf vendor/images
@@ -36,16 +36,9 @@ rm -rf vendor/gapps
 
 git clone https://github.com/stebomurkn420/Bonito -b havoc device/google/bonito
 git clone https://android.googlesource.com/device/sample device/sample
-git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r12 device/google/bonito-sepolicy
-git clone https://GitHub.com/gahsrom/vendor_google vendor/google
-git clone https://github.com/stebomurkn420/kernel_google_b4s4 kernel/google/b4s4
-git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_display hardware/qcom/sdm710/display
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_gps hardware/qcom/sdm710/gps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_media hardware/qcom/sdm710/media
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r47 hardware/qcom/sdm710/thermal
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r47 hardware/qcom/sdm710/vr
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_data_ipacfg-mgr -b bliss hardware/qcom/sdm710/data/ipacfg-mgr
+git clone https://github.com/shagbag913/android_vendor_google vendor/google
+git clone https://github.com/LineageOS/android_kernel_google_msm-4.9 -b lineage-17.1 kernel/google/msm-4.9
+git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.1 vendor/gapps
 
 . build/envsetup.sh
 
@@ -53,4 +46,6 @@ export USE_CCACHE=1
 export CCACHE_COMPRESS=1
 export CCACHE_EXEC=/usr/bin/ccache
 
-brunch bonito |& tee /tmp/build.log
+lunch havoc_bonito-userdebug
+
+make bacon -j$(nproc --all) |& tee /tmp/build.log
