bb09145643fb771def62b4827127a5d3a3f930a7
stebomurkn420 <stevn4127@gmail.com>
2020-01-18 07:19:29 -0500

    Update a few scripts

--- bliss-b4s4.sh
+++ bliss-b4s4.sh
@@ -32,16 +32,11 @@ rm -rf vendor/images
 rm -rf vendor/gapps
 
 git clone https://github.com/stebomurkn420/Bonito -b bliss device/google/bonito
-git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r12 device/google/bonito-sepolicy
-git clone https://github.com/TheMuppets/proprietary_vendor_google vendor/google
-git clone https://github.com/LineageOS/android_kernel_google_msm-4.9 kernel/google/msm-4.9
-git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_display hardware/qcom/sdm710/display
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_gps hardware/qcom/sdm710/gps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_media hardware/qcom/sdm710/media
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r47 hardware/qcom/sdm710/thermal
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r47 hardware/qcom/sdm710/vr
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_data_ipacfg-mgr -b bliss hardware/qcom/sdm710/data/ipacfg-mgr
+git clone https://android.googlesource.com/device/sample device/sample
+git clone https://github.com/shagbag913/android_vendor_google vendor/google
+git clone https://github.com/stebomurkn420/kernel_google_b4s4 -b upstream kernel/google/b4s4
+git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.1 vendor/gapps
+git clone https://github.com/LineageOS/android_hardware_qcom_sdm845_display hardware/qcom/sdm845/display
 
 cd $HOME/bliss
 
--- git.sh
+++ git.sh
@@ -6,20 +6,15 @@ rm -rf device/google/bonito
 rm -rf device/google/bonito-kernel
 rm -rf device/google/sargo
 rm -rf device/google/bonito-sepolicy
-rm -rf hardware/qcom/sdm845
 rm -rf kernel/google/bonito
-#rm -rf vendor/google
+rm -rf vendor/google
 rm -rf vendor/images
-#rm -rf vendor/gapps
+rm -rf vendor/gapps
+rm -rf hardware/qcom/sdm845/display
 
-git clone https://github.com/stebomurkn420/Bonito -b bliss device/google/bonito
-git clone https://android.googlesource.com/device/google/bonito-sepolicy -b android-10.0.0_r20 device/google/bonito-sepolicy
-git clone https://github.com/GahsROM/vendor_google vendor/google
-git clone https://github.com/stebomurkn420/kernel_google_b4s4 kernel/google/b4s4
-git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.0 vendor/gapps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_display hardware/qcom/sdm710/display
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_gps hardware/qcom/sdm710/gps
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_media hardware/qcom/sdm710/media
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/thermal -b android-9.0.0_r47 hardware/qcom/sdm710/thermal
-git clone https://android.googlesource.com/platform/hardware/qcom/sdm710/vr -b android-9.0.0_r47 hardware/qcom/sdm710/vr
-git clone https://github.com/stebomurkn420/sdm710_hardware_qcom_data_ipacfg-mgr -b bliss hardware/qcom/sdm710/data/ipacfg-mgr
+git clone https://github.com/stebomurkn420/Bonito -b havoc device/google/bonito
+git clone https://android.googlesource.com/device/sample device/sample
+git clone https://github.com/shagbag913/android_vendor_google vendor/google
+git clone https://github.com/stebomurkn420/kernel_google_b4s4 -b upstream kernel/google/b4s4
+git clone https://gitlab.com/shagbag913/vendor_gapps.git -b lineage-17.1 vendor/gapps
+git clone https://github.com/LineageOS/android_hardware_qcom_sdm845_display hardware/qcom/sdm845/display
