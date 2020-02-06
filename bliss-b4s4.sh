e4bb2cf0539dda86ef4e059e9a764016b1df85c8
stebomurkn420 <stevn4127@gmail.com>
2020-02-06 10:58:58 -0500

    Update bliss script

--- bliss-b4s4.sh
+++ bliss-b4s4.sh
@@ -2,7 +2,7 @@
 
 cd
 
-bash <(curl https://raw.githubusercontent.com/stebomurkn420/scripts/e7bf83207ec633aa5327415b4f0c58df1bdfd1b7/install-build-tools.sh)
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk adb fastboot
 
 cd
 
@@ -18,7 +18,7 @@ cd $HOME/bliss
 
 repo init -u https://github.com/BlissRoms/platform_manifest.git -b q
 
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
+repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j16
 
 rm -rf device/google/bonito
 rm -rf device/google/bonito-kernel
@@ -41,5 +41,11 @@ git clone https://github.com/LineageOS/android_hardware_qcom_sdm845_display hard
 cd $HOME/bliss
 
 . build/envsetup.sh
+
 lunch bliss_bonito-userdebug
+
+make blissify -j32 |& tee /tmp/build.log
+
+lunch bliss_sargo-userdebug
+
 make blissify -j32 |& tee /tmp/build.log
