0542269619fa7fc885f68ce32b7a7407484d8de3
stebomurkn420 <stevn4127@gmail.com>
2020-04-10 09:45:30 -0400

    Update bliss script

--- bliss-b4s4.sh
+++ bliss-b4s4.sh
@@ -29,31 +29,15 @@ rm -rf vendor/gapps
 
 repo init -u https://github.com/BlissRoms/platform_manifest.git -b q
 
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j16
-
-rm -rf device/google/bonito
-rm -rf device/google/bonito-kernel
-rm -rf device/google/sargo
-rm -rf device/google/bonito-sepolicy
-rm -rf hardware/qcom/sdm710
-rm -rf hardware/qcom/sdm845/display
-rm -rf kernel/google/bonito
-rm -rf vendor/google
-rm -rf vendor/images
-rm -rf vendor/gapps
-
-git clone https://github.com/BlissRoms-Devices/android_device_google_bonito -b q device/google/bonito
-#git clone https://android.googlesource.com/device/sample device/sample
-git clone https://github.com/BlissRoms-Devices/proprietary_android_vendor_google vendor/google
-git clone https://github.com/BlissRoms-Devices/android_kernel_google_b4s4 -b q kernel/google/b4s4
-git clone https://gitlab.com/shagbag913/vendor_gapps -b ten vendor/gapps
-git clone https://github.com/LineageOS/android_hardware_qcom_sdm845_display -b lineage-17.1 hardware/qcom/sdm845/display
+repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j32
 
 cd $HOME/bliss
 
 . build/envsetup.sh
 
 lunch bliss_bonito-userdebug
+
+repo sync
 
 make blissify |& tee /tmp/build.log
 
