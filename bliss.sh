17611d208b2adca562d4d0a860e67293f337a632
stevn4127 <stevn4127@gmail.com>
2021-04-13 17:43:25 -0400

    install libncurses5

--- bliss.sh
+++ bliss.sh
@@ -4,7 +4,7 @@ cd
 
 echo "Installing build tools to compile the ROM"
 sleep 5
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso gnupg python
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso gnupg python libncurses5
 echo "Installing build tools COMPLETED"
 sleep 5
 
